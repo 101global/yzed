0981bbd06414b2cb4916bd2a9c918a88bd3e4600
Theran <theran.brigowatz@gmail.com>
2020-06-25 11:58:43 +0900

    updated route

--- pages/kaimin.js
+++ pages/kaimin.js
@@ -34,7 +34,7 @@ const Reserved = ({ product }) => {
 
 export async function getStaticProps() {
   const product = await fetch(
-    `https://firestore.googleapis.com/v1/projects/yzed-88819/databases/(default)/documents/landingBrand/${process.env.brandLandingReserved}`,
+    `https://firestore.googleapis.com/v1/projects/yzed-88819/databases/(default)/documents/landingBrand/${process.env.brandLandingKaimin}`,
     { cors: 'no-cors' }
   ).then((res) => res.json().then((data) => data));
 
