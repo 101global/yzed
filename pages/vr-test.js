a2e4352435df41621055324bb8a2e3c01cd705e6
Theran <theran.brigowatz@gmail.com>
2020-07-03 16:06:33 +0900

    basic vr component

---
+++ pages/vr-test.js
@@ -0,0 +1,13 @@
+import React, { useContext, useEffect, useState } from 'react';
+
+import VRComponent from '../components/reusableStyledComponents/modelViewers/VR';
+
+const VRTest = () => {
+  return (
+    <div>
+      <VRComponent />
+    </div>
+  );
+};
+
+export default VRTest;
