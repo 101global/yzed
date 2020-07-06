2a46f4c9da9e49d4dd1079ec833d4cd2e32eb4c3
Theran <theran.brigowatz@gmail.com>
2020-07-06 17:33:53 +0900

    div styling

--- components/reusableStyledComponents/ModelViewers/VR.js
+++ components/reusableStyledComponents/ModelViewers/VR.js
@@ -29,66 +29,64 @@ const VRComponent = () => {
       </Head>
 
       {appRendered && (
-        <a-scene>
-          <a-assets>
-            <img id='groundTexture' src='https://cdn.aframe.io/a-painter/images/floor.jpg' />
-            <img id='skyTexture' src='https://cdn.aframe.io/a-painter/images/sky.jpg' />{' '}
-            <video
-              id='walking-video'
-              src='https://oneoone-resource.s3.ap-northeast-2.amazonaws.com/yzed/HQ_video_main_2_LOWER.mp4'
-              color='#fff'></video>
-            <a-asset-item
-              id='animatedModel'
-              src='https://oneoone-resource.s3.ap-northeast-2.amazonaws.com/yzed/FORTHERAN_JULY6.gltf'></a-asset-item>
-          </a-assets>
-          <a-entity
-            gltf-model='url(https://oneoone-resource.s3-ap-northeast-2.amazonaws.com/yzed/bvQP164cIw.gltf)'
-            scale='1 1 1'
-            position='0 0 -2.5'></a-entity>
-          <a-entity
-            gltf-model='url(https://oneoone-resource.s3.ap-northeast-2.amazonaws.com/yzed/FORTHERAN_JULY6.gltf)'
-            scale='1 1 1'
-            position='-2.5 0 0'
-            animation-mixer='clip: *; repetitions: 3'
-            rotation='0 90 0'></a-entity>
-          <a-entity
-            gltf-model='url(https://oneoone-resource.s3.ap-northeast-2.amazonaws.com/yzed/FORTHERAN_JULY6.gltf)'
-            scale='1 1 1'
-            position='0 0 2.5'
-            animation-mixer='clip: *'
-            rotation='0 180 0'></a-entity>
-          <a-entity
-            gltf-model='url(https://oneoone-resource.s3.ap-northeast-2.amazonaws.com/yzed/cube_version14.glb)'
-            scale='0.1 0.1 0.1'
-            position='2 2 0'
-            animation-mixer='clip: *'
-            rotation='0 180 0'></a-entity>
-          <a-sky
-            src='#skyTexture'
-            height='2048'
-            radius='30'
-            src='#skyTexture'
-            theta-length='90'
-            width='2048'></a-sky>
-          <a-plane rotation='-90 0 0' width='100' height='100' src='#groundTexture'></a-plane>
-          <a-light type='ambient' color='#fff'></a-light>
-          <a-entity color='#8498b2' instensity='2' position='-5 5 8'></a-entity>
-          <a-light type='point' intensity='3' position='2 4 4'></a-light>
-          <a-camera>
-            <a-cursor
-              id='cursor'
-              animation__click='property: scale; from: 0.1 0.1 0.1; to: 1 1 1; easing: easeInCubic; dur: 150; startEvents: click'
-              animation__clickreset='property: scale; to: 0.1 0.1 0.1; dur: 1; startEvents: animationcomplete__click'
-              animation__fusing='property: scale; from: 1 1 1; to: 0.1 0.1 0.1; easing: easeInCubic; dur: 150; startEvents: fusing'></a-cursor>
-          </a-camera>
-        </a-scene>
+        <div style={{ position: 'absolute', height: '100%', width: '100%' }}>
+          <a-scene>
+            <a-assets>
+              <img id='groundTexture' src='https://cdn.aframe.io/a-painter/images/floor.jpg' />
+              <img id='skyTexture' src='https://cdn.aframe.io/a-painter/images/sky.jpg' />{' '}
+              <video
+                id='walking-video'
+                src='https://oneoone-resource.s3.ap-northeast-2.amazonaws.com/yzed/HQ_video_main_2_LOWER.mp4'
+                color='#fff'></video>
+              <a-asset-item
+                id='animatedModel'
+                src='https://oneoone-resource.s3.ap-northeast-2.amazonaws.com/yzed/FORTHERAN_JULY6.gltf'></a-asset-item>
+            </a-assets>
+            <a-entity
+              gltf-model='url(https://oneoone-resource.s3-ap-northeast-2.amazonaws.com/yzed/bvQP164cIw.gltf)'
+              scale='1 1 1'
+              position='0 0 -2.5'></a-entity>
+            <a-entity
+              gltf-model='url(https://oneoone-resource.s3.ap-northeast-2.amazonaws.com/yzed/FORTHERAN_JULY6.gltf)'
+              scale='1 1 1'
+              position='-2.5 0 0'
+              animation-mixer='clip: *; repetitions: 3'
+              rotation='0 90 0'></a-entity>
+            <a-entity
+              gltf-model='url(https://oneoone-resource.s3.ap-northeast-2.amazonaws.com/yzed/FORTHERAN_JULY6.gltf)'
+              scale='1 1 1'
+              position='0 0 2.5'
+              animation-mixer='clip: *'
+              rotation='0 180 0'></a-entity>
+            <a-entity
+              gltf-model='url(https://oneoone-resource.s3.ap-northeast-2.amazonaws.com/yzed/cube_version14.glb)'
+              scale='0.1 0.1 0.1'
+              position='2 2 0'
+              animation-mixer='clip: *'
+              rotation='0 180 0'></a-entity>
+            <a-sky
+              src='#skyTexture'
+              height='2048'
+              radius='30'
+              src='#skyTexture'
+              theta-length='90'
+              width='2048'></a-sky>
+            <a-plane rotation='-90 0 0' width='100' height='100' src='#groundTexture'></a-plane>
+            <a-light type='ambient' color='#fff'></a-light>
+            <a-entity color='#8498b2' instensity='2' position='-5 5 8'></a-entity>
+            <a-light type='point' intensity='3' position='2 4 4'></a-light>
+            <a-camera>
+              <a-cursor
+                id='cursor'
+                animation__click='property: scale; from: 0.1 0.1 0.1; to: 1 1 1; easing: easeInCubic; dur: 150; startEvents: click'
+                animation__clickreset='property: scale; to: 0.1 0.1 0.1; dur: 1; startEvents: animationcomplete__click'
+                animation__fusing='property: scale; from: 1 1 1; to: 0.1 0.1 0.1; easing: easeInCubic; dur: 150; startEvents: fusing'></a-cursor>
+            </a-camera>
+          </a-scene>
+        </div>
       )}
       <style jsx global>
-        {`
-          .a-enter-vr {
-            bottom: -30px;
-          }
-        `}
+        {``}
       </style>
     </>
   );
