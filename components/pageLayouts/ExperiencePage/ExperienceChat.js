90622ab50dbb8d21ddd6a08c14555d0f15bc7641
Theran <theran.brigowatz@gmail.com>
2020-07-08 17:29:58 +0900

    basic message data

--- components/pageLayouts/ExperiencePage/ExperienceChat.js
+++ components/pageLayouts/ExperiencePage/ExperienceChat.js
@@ -1,11 +1,42 @@
-import React, {useContext, useEffect, useState} from 'react'
+import React, { useContext, useEffect, useState } from 'react';
+import userIcon from '../../../public/account-icon.svg';
 
+const messages = [
+  {
+    user: {
+      userName: 'Theran',
+      avatar:
+        'https://www.pikpng.com/pngl/m/80-805523_default-avatar-svg-png-icon-free-download-264157.png',
+    },
+    content: 'Hello',
+  },
+];
 const ExperienceChat = () => {
   return (
-    <div>
-      <h1>Chat</h1>
-    </div>
-  )
-}
+    <>
+      <div>
+        {messages.map((message) => (
+          <>
+            <img src={userIcon} />
+            <p>
+              <strong>{message.user.userName}</strong>
+              {message.content}
+            </p>
+          </>
+        ))}
+        <input />
+      </div>
+      <style jsx>{`
+
+        img {
+          height: 20px;
+        }
+        strong {
+          margin-right: 10px;
+        }
+      `}</style>
+    </>
+  );
+};
 
 export default ExperienceChat;
