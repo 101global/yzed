57ceb289f8351d0c0353016270f3312f915b93a4
Theran <theran.brigowatz@gmail.com>
2020-07-29 15:37:23 +0900

    updated text with context

--- __tests__/model_viewer_snapshot.test.js
+++ __tests__/model_viewer_snapshot.test.js
@@ -6,17 +6,20 @@ import React from 'react';
 import StationaryModelViewer from '../components/ReusableComponents/ModelViewers/StationaryModelViewer';
 import { model } from '../__mocks__/dataMock';
 import renderer from 'react-test-renderer';
+import { ThemeContext } from '../utilities/context/ThemeContext';
 
 const mockSetTopModelDark = () => {};
 
 it('renders model viewer controller unchanged', () => {
   const wrapper = renderer
     .create(
-      <ModelViewerController
-        model={model}
-        topModelDark={true}
-        setTopModelDark={mockSetTopModelDark}
-      />
+      <ThemeContext.Provider value={{ darkMode: false }}>
+        <ModelViewerController
+          model={model}
+          topModelDark={true}
+          setTopModelDark={mockSetTopModelDark}
+        />
+      </ThemeContext.Provider>
     )
     .toJSON();
   expect(wrapper).toMatchSnapshot();
@@ -25,11 +28,13 @@ it('renders model viewer controller unchanged', () => {
 it('renders stationary model viewer unchanged', () => {
   const wrapper = renderer
     .create(
-      <StationaryModelViewer
-        model={model}
-        topModelDark={true}
-        setTopModelDark={mockSetTopModelDark}
-      />
+      <ThemeContext.Provider value={{ darkMode: false }}>
+        <StationaryModelViewer
+          model={model}
+          topModelDark={true}
+          setTopModelDark={mockSetTopModelDark}
+        />
+      </ThemeContext.Provider>
     )
     .toJSON();
   expect(wrapper).toMatchSnapshot();
@@ -38,11 +43,13 @@ it('renders stationary model viewer unchanged', () => {
 it('renders animated model viewer unchanged', () => {
   const wrapper = renderer
     .create(
-      <AnimatedModelViewer
-        model={model}
-        topModelDark={true}
-        setTopModelDark={mockSetTopModelDark}
-      />
+      <ThemeContext.Provider value={{ darkMode: false }}>
+        <AnimatedModelViewer
+          model={model}
+          topModelDark={true}
+          setTopModelDark={mockSetTopModelDark}
+        />
+      </ThemeContext.Provider>
     )
     .toJSON();
   expect(wrapper).toMatchSnapshot();
