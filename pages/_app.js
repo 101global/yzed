3b3416e8064070669b31310c0c22d6629c8f540e
Theran <theran.brigowatz@gmail.com>
2020-07-29 15:24:49 +0900

    organized meta

--- pages/_app.js
+++ pages/_app.js
@@ -21,12 +21,12 @@ function MyApp({ Component, pageProps, user }) {
         <link rel='apple-touch-icon' sizes='180x180' href='apple-touch-icon.png' />
         <link rel='icon' type='image/png' sizes='32x32' href='favicon-32x32.png' />
         <link rel='icon' type='image/png' sizes='16x16' href='favicon-16x16.png' />
+        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
+        <meta name='description' content='YZED: A unique AR experience'></meta>
         <meta charSet='utf-8' />
         <meta name='theme-color' content='#0D0D0D' />
         <meta property='og:type' content='website' />
-        <meta name='viewport' content='initial-scale=1.0, width=device-width' />
-        <meta name='description' content='YZED: A unique AR experience'></meta>
-        <meta property='og:url' content='https:/yzed.me' />
+        <meta property='og:url' content='https://yzed.me' />
         <meta property='og:title' content='YZED: A unique AR experience' />
         <meta property='og:description' content='YZED: Check out unique 3D and AR fashion.' />
         <meta
